842ee8ff706686e48161994b02327bd82418bec2
Tabata <tabatac.souto@gmail.com>
2022-07-21 00:16:53 -0300

    feat: função que retorna o dia e hora atualizado.

---
+++ src/helpers/currentDate.js
@@ -0,0 +1,11 @@
+const currentDate = () => {
+  const date = new Date();
+  const day = date.getDate();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+  const hour = date.toLocaleTimeString('pt', { hour24: true });
+
+  return `${day}/${month}/${year} - ${hour}`;
+};
+
+export default currentDate;
