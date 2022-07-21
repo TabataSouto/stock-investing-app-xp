3d5b8477af91d3aabf0420f1bdcb35cd832d0ba5
Tabata <tabatac.souto@gmail.com>
2022-07-20 23:22:39 -0300

    feat: criação component Buy para realização da compra de uma ação.

---
+++ src/components/BuyAndSell/Buy.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addQtde, calcPurchase } from '../../redux/reducers/buyAsset';
+import { isLeveraged } from '../../redux/reducers/account';
+
+import Input from '../Input';
+import Style from './Style';
+
+function Buy() {
+  const dispatch = useDispatch();
+  const toBuy = useSelector((state) => ({
+    asset: state.negotiation.asset[0].amount,
+    buy: state.buy.calc,
+    sell: state.sell.qtde,
+  }));
+  const [orderQtde, setOrderQtde] = useState({ qtde: 0, value: toBuy.asset });
+
+  useEffect(() => {
+    dispatch(addQtde(orderQtde));
+    dispatch(calcPurchase(orderQtde));
+    dispatch(isLeveraged(toBuy.buy));
+  }, [orderQtde, toBuy]);
+
+  const handleChange = ({ target: { value } }) => {
+    setOrderQtde({ ...orderQtde, qtde: value });
+  };
+
+  return (
+    <Style.Div>
+      <p>Comprar</p>
+      <Input
+        id="buy-asset"
+        type="number"
+        name="buy"
+        value={orderQtde.qtde}
+        placeholder="Informe a quantidade"
+        onChange={handleChange}
+        disabled={toBuy.sell !== 0 && true}
+      />
+    </Style.Div>
+  );
+}
+
+export default Buy;
