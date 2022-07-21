0c72a437d81d16a6c5c70fdfe3ede53575c5a90c
Tabata <tabatac.souto@gmail.com>
2022-07-20 22:50:30 -0300

    fix: rotas que ainda não foram criadas comentadas, pois estavam quebrando a aplicação.

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,18 +3,18 @@ import { Routes, Route } from 'react-router-dom';
 
 import Login from '../pages/Login';
 import Assets from '../pages/Assets';
-import Negotiation from '../pages/Negotiation';
-import Orders from '../pages/Orders';
-import FinancialTransaction from '../pages/FinancialTransaction';
+// import Negotiation from '../pages/Negotiation';
+// import Orders from '../pages/Orders';
+// import FinancialTransaction from '../pages/FinancialTransaction';
 
 function Router() {
   return (
     <Routes>
       <Route exact path="/" element={<Login />} />
       <Route path="assets" element={<Assets />} />
-      <Route path="negotiation/:id" element={<Negotiation />} />
-      <Route path="orders" element={<Orders />} />
-      <Route path="financial-transaction" element={<FinancialTransaction />} />
+      {/* <Route path="negotiation/:id" element={<Negotiation />} /> */}
+      {/* <Route path="orders" element={<Orders />} /> */}
+      {/* <Route path="financial-transaction" element={<FinancialTransaction />} /> */}
     </Routes>
   );
 }
