d24f9c4a5f443ca4273f2d614ddb6d170abded08
Tabata <tabatac.souto@gmail.com>
2022-07-20 23:44:29 -0300

    feat: página para negociações (compra e venda de ação) finalizada.

---
+++ src/pages/Negotiation/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import BuyAndSell from '../../components/BuyAndSell';
+import Header from '../../components/Header';
+import Ticket from '../../components/Ticket';
+
+function Negotiation() {
+  return (
+    <section>
+      <Header />
+      <Ticket />
+      <BuyAndSell />
+    </section>
+  );
+}
+
+export default Negotiation;
